40c07e017a6ba05a102a8378ae55aacd3b57d222
phdljr <phdljr@naver.com>
2022-04-29 16:28:43 +0900

    from jr

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,24 +1,46 @@
 <template>
   <div class="signUpBg">
-    <img src="../assets/logo.png" width="100px">
+    <img src="../assets/logo.png" width="100px" />
     <b-form class="signUp" @submit="submitForm">
       <div class="inputBox">
-        <b-form-input type="email" v-model="registerData.email" placeholder="이메일을 입력해 주세요"/>
+        <b-form-input
+          type="email"
+          v-model="registerData.email"
+          placeholder="이메일을 입력해 주세요"
+        />
       </div>
       <div class="inputBox">
-        <b-form-input type="password" v-model="registerData.password" placeholder="비밀번호를 입력해 주세요"/>
+        <b-form-input
+          type="password"
+          v-model="registerData.password"
+          placeholder="비밀번호를 입력해 주세요"
+        />
       </div>
       <div class="inputBox">
-        <b-form-input type="password" v-model="passwordCheck" placeholder="비밀번호를 재입력해 주세요"/>
+        <b-form-input
+          type="password"
+          v-model="passwordCheck"
+          placeholder="비밀번호를 재입력해 주세요"
+        />
       </div>
       <div class="inputBox">
-        <b-form-input id="phone" type="tel" v-model="registerData.phoneNumber" placeholder="휴대폰 번호를 입력해 주세요" pattern="[0-9]{2,3}-[0-9]{3,4}-[0-9]{3,4}" @keyup="chkItemPhone"/>
+        <b-form-input
+          id="phone"
+          type="tel"
+          v-model="registerData.phoneNum"
+          placeholder="휴대폰 번호를 입력해 주세요"
+          pattern="[0-9]{2,3}-[0-9]{3,4}-[0-9]{3,4}"
+          @keyup="chkItemPhone"
+        />
       </div>
       <div class="inputBox">
-        <b-form-input type="text" v-model="phoneCheck" placeholder="인증번호를 입력해 주세요"/>
+        <b-form-input
+          type="text"
+          v-model="phoneCheck"
+          placeholder="인증번호를 입력해 주세요"
+        />
       </div>
-      <b-button id="loginButton" type="submit">
-      </b-button>
+      <b-button id="loginButton" type="submit"> 회원가입 </b-button>
     </b-form>
 
     <div class="Login">
@@ -29,44 +51,45 @@
 </template>
 
 <script>
-import axios from 'axios'
-
+import axios from "axios";
 
 export default {
-  name: 'SignUp',
+  name: "SignUp",
   props: {},
   data() {
     return {
-        registerData:{
-          email: '',
-          nickname: '',
-          password: '',
-          phoneNumber: null,
-        },
-        passwordCheck:'',
-        phoneCheck:''
-    }
+      registerData: {
+        email: "",
+        nickname: "",
+        password: "",
+        phoneNum: null,
+      },
+      passwordCheck: "",
+      phoneCheck: "",
+    };
   },
-  components: {
-  },
+  components: {},
   methods: {
     submitForm() {
-        //alert(this.id + this.password + this.nickname + this.age)
-        var url = '';
-        var signUpData = {
-          "user_id": this.id,
-          "user_pw": this.password,
-          "nickname": this.nickname,
-          "age": this.age,
-        }
-        axios.post(url, signUpData).then(function(res){
+      //alert(this.id + this.password + this.nickname + this.age)
+      var url = "http://202.31.200.215";
+      // var signUpData = {
+      //   user_email: this.user_email,
+      //   user_pw: this.user_pw,
+      //   user_nickname: this.user_nickname,
+      //   user_phoneNum: this.user_phoneNum,
+      // };
+      axios
+        .post(url + "/user", this.registerData)
+        .then(function (res) {
           alert(res);
-        }).catch(function(err){
-          alert(err)
         })
+        .catch(function (err) {
+          alert(err);
+        });
     },
     chkItemPhone() {
-      var temp = document.getElementById("phone").value
+      var temp = document.getElementById("phone").value;
       var number = temp.replace(/[^0-9]/g, "");
       var phone = "";
 
@@ -74,8 +97,8 @@ export default {
         return number;
       } else if (number.length < 10) {
         phone += number.substr(0, 2);
-            phone += "-";
-            phone += number.substr(2, 3);
+        phone += "-";
+        phone += number.substr(2, 3);
         phone += "-";
         phone += number.substr(5);
       } else if (number.length < 11) {
@@ -91,47 +114,46 @@ export default {
         phone += "-";
         phone += number.substr(7);
       }
-      document.getElementById("phone").value = phone
+      document.getElementById("phone").value = phone;
     },
-
   },
-}
+};
 </script>
 
 <style>
-#submit{
-    text-align: left;
+#submit {
+  text-align: left;
 }
-#submit label{
-    display: inline-block;
-    width: 100px;
+#submit label {
+  display: inline-block;
+  width: 100px;
 }
-body{
+body {
   margin: 0;
 }
-div{
+div {
   box-sizing: border-box;
 }
-.signUpBg{
+.signUpBg {
   width: 768px;
   background-color: rgb(245, 245, 245);
   margin: auto;
   border-radius: 8px;
   margin-top: 0px;
 }
-.signUp{
+.signUp {
   text-align: left;
   width: 400px;
-  margin:0 auto;
+  margin: 0 auto;
 }
-#loginButton{
+#loginButton {
   width: 100%;
   margin-top: 15px;
 }
-.inputBox{
+.inputBox {
   text-align: left;
   width: 100%;
-  margin:0 auto;
+  margin: 0 auto;
   background: rgba(223, 223, 223, 30%);
   border: rgba(80, 80, 80, 100%);
   border-radius: 6px;
@@ -139,12 +161,12 @@ div{
   margin-top: 10px;
   padding: 7px;
 }
-.inputBox input{
+.inputBox input {
   width: 100%;
   border: none;
   background: transparent;
 }
-.Login{
+.Login {
   padding: 20px;
   margin-top: 20px;
   border-top: solid rgb(190, 190, 190);
